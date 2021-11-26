f2377c4cf67d2ad9bbf0e273ac760287b735578f
SamiaNezlioui <84906495+SamiaNezlioui@users.noreply.github.com>
2021-11-26 13:09:24 +0100

    formulaire de publication des questions

---
+++ questions_publier.php
@@ -0,0 +1,31 @@
+<?php require('Actions/securityAction.php');?>
+
+<!DOCTYPE html>
+<html lang="en">
+<?php include 'includes/head.php';?>
+<body>
+    
+<br><br>
+    <form class="container" method="POST">
+
+        <?php if(isset($messageError)) {echo '<p>' . $messageError . '</p>';} ?>
+        
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Titre de la question</label>
+            <input type="text" class="form-control" name="title">
+        </div>
+        <br><br>
+        <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Description de la question</label>
+            <textarea class="form-control" name="description"></textarea>
+        </div>
+        <br><br>
+        <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Contenu de la question</label>
+            <textarea class="form-control" name="content"></textarea>
+        </div>
+        <br><br>
+        <button type="submit" class="btn btn-primary" name="validate">publier la question</button>
+    </form>
+</body>
+</html>
